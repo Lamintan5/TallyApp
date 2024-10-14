109d2a45a5451c78878122d36fabf024531290a5
Lamintan5 <tech@studio5ive.org>
2024-10-14 09:39:32 +0300

    supplier-search

--- lib/views/enty_tabs/suppliers.dart
+++ lib/views/enty_tabs/suppliers.dart
@@ -46,6 +46,8 @@ class _SuppliersState extends State<Suppliers> {
   List<String> admin = [];
   late bool _layout;
   bool _loading = false;
+  bool isFilled = false;
+
   int? sortColumnIndex;
   bool isAscending = false;
   String selectedID = "";
@@ -302,8 +304,11 @@ class _SuppliersState extends State<Suppliers> {
                         children: [
                           Row(
                             children: [
-                              Container(width: 300,
-                                padding: EdgeInsets.only(left: 10),
+                              Container(
+                                constraints: BoxConstraints(
+                                    maxWidth: 280,
+                                    minWidth: 100
+                                ),
                                 decoration: BoxDecoration(
                                   border: Border.all(
                                       width: 1, color: Colors.black
@@ -317,17 +322,45 @@ class _SuppliersState extends State<Suppliers> {
                                   keyboardType: TextInputType.text,
                                   style: TextStyle(color: Colors.black),
                                   decoration: InputDecoration(
-                                      hintText: "Search...",
-                                      hintStyle: TextStyle(color: secondaryColor),
-                                      filled: false,
-                                      isDense: true,
-                                      contentPadding: EdgeInsets.all(8),
-                                      icon: Icon(Icons.search, color: Colors.black,),
-                                      border: OutlineInputBorder(
-                                          borderSide: BorderSide.none
-                                      )
+                                    hintText: "Search",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(5)
+                                      ),
+                                      borderSide: BorderSide.none,
+                                    ),
+                                    hintStyle: TextStyle(color: secondaryColor, fontWeight: FontWeight.normal),
+                                    prefixIcon: Icon(CupertinoIcons.search, size: 20,color: secondaryColor),
+
+                                    prefixIconConstraints: BoxConstraints(
+                                        minWidth: 40,
+                                        minHeight: 30
+                                    ),
+                                    suffixIcon: isFilled?InkWell(
+                                        onTap: (){
+                                          _search.clear();
+                                          setState(() {
+                                            isFilled = false;
+                                          });
+                                        },
+                                        borderRadius: BorderRadius.circular(100),
+                                        child: Icon(Icons.cancel, size: 20,color: secondaryColor)
+                                    ) :SizedBox(),
+                                    suffixIconConstraints: BoxConstraints(
+                                        minWidth: 40,
+                                        minHeight: 30
+                                    ),
+                                    contentPadding: EdgeInsets.symmetric(vertical: 1, horizontal: 20),
+                                    filled: false,
+                                    isDense: true,
                                   ),
-                                  onChanged:  (value) => setState((){}),
+                                  onChanged:  (value) => setState((){
+                                    if(value.isNotEmpty){
+                                      isFilled = true;
+                                    } else {
+                                      isFilled = false;
+                                    }
+                                  }),
                                 ),
                               ),
                               Expanded(child: SizedBox()),
